5d00b4ae4fa4208c7606dadba33df64ec291c607
AnaCarolinaAndrade <ana_ca_araujo@estudante.sesisenai.org.br>
2024-08-23 17:16:14 -0300

    Criação do priemeiro projeto com react

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnívoros</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit commodi temporibus magni? Voluptatibus enim aut delectus esse temporibus similique blanditiis soluta? Unde modi corporis, facere repellat qui voluptates harum. Expedita.</p>
+    </>
+  )
+}
+
+export default App
