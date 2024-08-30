fb961862b3fd0b3c2f322f161427dec69f48b8cc
ANA CAROLINA ANDRADE DE ARAUJO <ana_ca_araujo@estudante.sesisenai.org.br>
2024-08-30 17:18:08 -0300

    fizemos um mini projetinho com css

---
+++ componentesVerdade/src/Componets/Corpo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Corpo() {
+  return (
+    <div>
+        <h3>Teste rapido no react ;D</h3>
+    <img src="./ImgDragaoo/dragao.png" alt="" />
+    </div>
+  )
+}
+
+export default Corpo
